ccc597955628f37ae72179aecdfe50acbbf9baa8
admin <admin@admins-MacBook-Pro.local>
2019-11-25 08:56:12 -0600

    questionably created game logic

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -1,12 +1,23 @@
 //&&
 var cards = ["queen","queen","king","king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User filpped" + cardsInPlay)
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User filpped" + " " + cardsInPlay)
+
+function checkForMatch(){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+}
+};
+//unclear on the correct syntax for the following if statement
+function flipCards(cardId){
 if (cardsInPlay.length===2 && cardsInPlay[0]===cardsInPlay[1]) {
 	alert("You found a match!");
 } else {alert("Sorry, try again.")}
+//are the following three placed correctly in this function? 
+console.log("User flipped" + cards[cardId]);
+cardsInPlay.push(cards[cardId]);
+checkForMatch();
+};
+flipCards(0);
+flipCards(2);
